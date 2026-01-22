686ffcdfab6410fdba5a243827835f12a0173bc9
Maddie <maddie@eaiti.com>
2026-01-22 13:53:57 -0500

    fix conflict

--- material/script.ts
+++ material/script.ts
@@ -7,7 +7,12 @@ class Script {
   static greetUser(name: string) {
     console.log(`Hello, ${name}! Welcome to our application.`);
   }
+
+  static newMethod() {
+    console.log("This is a new method added to the script.");
+  }
 }
 
 Script.printTimeStamp();
 Script.greetUser("Alice");
+Script.newMethod();
